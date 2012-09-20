5ac1aa05c3d213ecd90468825616c8fa30ba131e
Arvesh <arvesh9@gmail.com>
2012-09-20 15:08:58 +0400

    updated customer.php

--- application/controllers/customer.php
+++ application/controllers/customer.php
@@ -1,36 +1,72 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-/**
-* @author CodeBallistic Admin
-*/
-class Customer extends CI_Controller{
-	
-	function index(){
-		$data = array();
-		$data['main_content'] = 'customer';
-		$this->load->view('includes/template',$data);
-	}
-
-	function login($data){
-		
-	}
-
-	function logout(){
-		
-	}
-
-	function project($id = ""){
-		
-	}
-
-	function update_project($id,$data){
-		
-	}
-
-	function update($data){
-		
-	}
-
-
-
-}
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+
+/**
+*
+* @author CodeBallistic Admin
+*
+*/
+
+class Customer extends CI_Controller{
+
+	
+
+	function index(){
+
+		$data = array();
+
+		$data['main_content'] = 'customer';
+
+		$this->load->view('includes/template',$data);
+
+	}
+
+
+
+	function login($data){
+
+		
+
+	}
+
+
+
+	function logout(){
+
+		
+
+	}
+
+
+
+	function project($id = ""){
+
+		
+
+	}
+
+
+
+	function update_project($id,$data){
+
+		
+
+	}
+
+
+
+	function update($data){
+
+		
+
+	}
+
+
+
+
+
+
+
+}
+
